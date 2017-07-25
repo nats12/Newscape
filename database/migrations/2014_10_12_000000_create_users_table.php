d75d143d3ec0f0f73da6006efaecaaec3f827a50
Natalie Mclaren <natalie_mclaren93@hotmail.com>
2017-07-25 19:03:03 +0200

    Add user twitter handle to migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('twitter_id')->unique();
+            $table->string('handle');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
