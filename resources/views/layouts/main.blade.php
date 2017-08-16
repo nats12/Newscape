12c1a15c506a711df56ab1750441a861289355dd
Jason Lai <jason@cyber-duck.co.uk>
2017-08-16 11:16:31 +0100

    og tags updated

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,28 +5,17 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        @include('../partials/_metatags')
+
         <link rel="icon" type="image/png" href="/images/favicon-32x32.png" sizes="32x32">
         <link rel="icon" type="image/png" href="/images/favicon-96x96.png" sizes="96x96">
         <link rel="icon" type="image/png" href="/images/favicon-16x16.png" sizes="16x16">
-
+        
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>@yield('title')</title>
-        <meta name="description" content="Newscape fuses Twitter & News APIs to give users a personalised dashboard of their favourite news sources alongside their Twitter feed.">
-        <!-- OG meta tags -->
-        <meta property="og:type" content="website"/>
-        <meta property="og:title" content="Newscape: a personalised dashboard of your favourite news sources"/>
-        <meta property="og:url" content="http://newscape.herokuapp.com"/>
-        <meta property="og:description" content="Newscape fuses Twitter & News APIs to give users a personalised dashboard of their favourite news sources alongside their Twitter feed."/>
-        <meta property="og:image" content="http://newscape.herokuapp.com/images/readme/newscape.png"/>
-        <!-- Twitter meta tags -->
-        <meta name="twitter:card" content="summary_large_image">
-        <meta name="twitter:creator" content="@LaiJase">
-        <meta name="twitter:creator" content="@nats_369">
-        <meta name="twitter:title" content="Newscape: a personalised dashboard of your favourite news sources">
-        <meta name="twitter:description" content="Newscape fuses Twitter & News APIs to give users a personalised dashboard of their favourite news sources alongside their Twitter feed.">
-        <meta name="twitter:image" content="http://newscape.herokuapp.com/images/readme/newscape.png">
+        
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 
---
+++ resources/views/partials/_metatags.blade.php
@@ -0,0 +1,20 @@
+<meta name="description" content="Newscape fuses Twitter & News APIs to give users a personalised dashboard of their favourite news sources alongside their Twitter feed.">
+
+<!-- OG meta tags -->
+<meta property="og:locale" content="en_GB"/>
+<meta property="og:type" content="website"/>
+<meta property="og:title" content="Newscape: Personalise Your Newsfeed"/>
+<meta property="og:site_name" content="Newscape"/>
+<meta property="og:url" content="http://newscape.herokuapp.com"/>
+<meta property="og:description" content="Newscape fuses Twitter & News APIs to give users a personalised dashboard of their favourite news sources alongside their Twitter feed."/>
+<meta property="og:image" content="http://newscape.herokuapp.com/images/readme/newscape.png"/>
+<meta property="og:image:width" content="1280"/>
+<meta property="og:image:height" content="726"/>
+
+<!-- Twitter meta tags -->
+<meta name="twitter:card" content="summary_large_image"/>
+<meta name="twitter:creator" content="@LaiJase"/>
+<meta name="twitter:creator" content="@nats_369"/>
+<meta name="twitter:title" content="Newscape: Personalise Your Newsfeed"/>
+<meta name="twitter:description" content="Newscape fuses Twitter & News APIs to give users a personalised dashboard of their favourite news sources alongside their Twitter feed."/>
+<meta name="twitter:image" content="http://newscape.herokuapp.com/images/readme/newscape.png"/>
