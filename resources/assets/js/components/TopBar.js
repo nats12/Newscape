6cbf636147f734dab441d28d7e0c26c0ac526765
Natalie Mclaren <natalie_mclaren93@hotmail.com>
2017-09-17 11:19:23 +0200

    Add descriptive subtitle

--- resources/assets/js/components/TopBar.js
+++ resources/assets/js/components/TopBar.js
@@ -12,6 +12,7 @@ class TopBar extends Component {
                     <div className="top-bar-left">
                         <ul className="menu">
                             <li className="menu-text"><h1>Newscape</h1></li>
+                            <li className="menu-text strapline"><small>Personalise your newsfeed.</small></li>
                         </ul>
                     </div>
                     {/* Add github icon here */ }
